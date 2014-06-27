18c485957465198d56846a5815503f36028005d5
Mark Holland <mark@partiallogic.com>
2014-06-27 18:27:49 +1200

    Created project

---
+++ app/models/post.js
@@ -0,0 +1,11 @@
+// app/models/post.js
+
+var mongoose	= require('mongoose');
+var Schema		= mongoose.Schema;
+
+var PostSchema	= new Schema({
+	name: String
+});
+
+module.exports = mongoose.model('Post', PostSchema);
+
---
+++ server.js
@@ -0,0 +1,106 @@
+// server.js
+
+var express 	= require('express');
+var app			= express();
+var bodyParser	= require('body-parser');
+var mongoose	= require('mongoose');
+var Post		= require('./app/models/post');
+
+// URI of db
+mongoose.connect('mongodb://test:test@ds061228.mongolab.com:61228/test-api');
+
+app.use(bodyParser());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+// middleware
+router.use(function(req, res, next) {
+	// logging
+	console.log('Something is happening.');
+	next(); // go to next routes
+});
+
+// test route
+router.get('/', function(req,res) {
+	res.json({ message: 'Welcome to api.'});
+});
+
+// Post routes
+router.route('/posts')
+
+	// create a post
+	.post(function(req, res) {
+
+		var post = new Post();
+		post.name = req.body.name;
+
+		post.save(function(err) {
+			if(err)
+				res.send(err);
+
+			res.json({message: 'Post created.'});
+		});
+	})
+
+	// get all posts
+	.get(function(req, res) {
+		Post.find(function(err, posts) {
+			if(err)
+				res.send(err);
+
+			res.json(posts);
+		});
+	});
+
+router.route('/posts/:post_id')
+
+	.get(function(req, res) {
+		Post.findById(req.params.post_id, function(err, post) {
+			if(err)
+				res.send(err);
+
+			res.json(post);
+		});
+	})
+
+	.put(function(req, res) {
+
+		// find post
+		Post.findById(req.params.post_id, function(err, post) {
+
+			if(err)
+				res.send(err);
+		
+			// update name
+			post.name = req.body.name;
+
+			// save the post
+			post.save(function(err) {
+				if(err)
+					res.send(err);
+
+				res.json({ message: 'Post updated.'})
+			});
+		});
+	})
+
+	.delete(function(req, res) {
+		Post.remove({
+			_id: req.params.post_id
+		}, function(err, post) {
+			if(err)
+				res.send(err);
+
+			res.json({message: 'Deleted post with id: '+req.params.post_id});
+		});
+	});
+
+// register routes
+app.use('/api', router);
+
+
+// start server
+app.listen(port);
+console.log('listening on port: ' + port);
